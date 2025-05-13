b0d2a874b52f6cd2f55ad811d1a907be43ee8a41
Timothy Wu <timothytwu@ucsb.edu>
2025-05-12 18:45:08 -0700

    timothy - add admin requests page tests still pending

---
+++ frontend/src/tests/pages/AdminRequestsPage.test.js
@@ -0,0 +1,72 @@
+import { render, waitFor, screen } from "@testing-library/react";
+import { QueryClient, QueryClientProvider } from "react-query";
+import { MemoryRouter } from "react-router-dom";
+import AdminRequestsPage from "main/pages/AdminRequestsPage";
+import { recommendationRequestFixtures } from "fixtures/recommendationRequestFixtures";
+import { systemInfoFixtures } from "fixtures/systemInfoFixtures";
+import mockConsole from "jest-mock-console";
+
+import axios from "axios";
+import AxiosMockAdapter from "axios-mock-adapter";
+
+describe("AdminRequestsPage tests", () => {
+  const axiosMock = new AxiosMockAdapter(axios);
+
+  const testId = "RequestsTable";
+
+  beforeEach(() => {
+    axiosMock.reset();
+    axiosMock.resetHistory();
+    axiosMock
+      .onGet("/api/admin/requests")
+      .reply(200, recommendationRequestFixtures.threeRequests);
+    axiosMock
+      .onGet("/api/systemInfo")
+      .reply(200, systemInfoFixtures.showingNeither);
+  });
+
+  test("renders without crashing on three requests", async () => {
+    const queryClient = new QueryClient();
+    axiosMock.onGet("/api/admin/requests").reply(200, recommendationRequestFixtures.threeRequests);
+
+    render(
+      <QueryClientProvider client={queryClient}>
+        <MemoryRouter>
+          <AdminRequestsPage />
+        </MemoryRouter>
+      </QueryClientProvider>,
+    );
+    await screen.findByText("Requests");
+  });
+
+  test("renders empty table when backend unavailable", async () => {
+    const queryClient = new QueryClient();
+
+    axiosMock.onGet("/api/admin/requests").timeout();
+
+    const restoreConsole = mockConsole();
+
+    render(
+      <QueryClientProvider client={queryClient}>
+        <MemoryRouter>
+          <AdminRequestsPage />
+        </MemoryRouter>
+      </QueryClientProvider>,
+    );
+
+    await waitFor(() => {
+      expect(axiosMock.history.get.length).toBeGreaterThanOrEqual(1);
+    });
+
+    const errorMessage = console.error.mock.calls[0][0];
+    expect(errorMessage).toMatch(
+      "Error communicating with backend via GET on /api/admin/requests",
+    );
+    restoreConsole();
+
+    expect(
+      screen.queryByTestId(`${testId}-cell-row-0-col-id`),
+    ).not.toBeInTheDocument();
+  });
+});
+
